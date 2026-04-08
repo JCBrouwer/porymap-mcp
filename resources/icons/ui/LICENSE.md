cd60ae01b02b6669cae0d23e2aed8ca820249842
Hans <hans@brouwer.work>
2026-04-08 20:46:17 +0200

    move porymap source to submodule

--- resources/icons/ui/LICENSE.md
+++
@@ -1,3 +0,0 @@
-The icons in this folder are slightly modified from QDarkStyleSheet 
-by Colin Duquesnoy and are licensed under 
-[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
--- src/settings.cpp
+++
@@ -1,9 +0,0 @@
-#include "settings.h"
-
-Settings::Settings()
-{
-    this->smartPathsEnabled = false;
-    this->betterCursors = true;
-    this->playerViewRectEnabled = false;
-    this->cursorTileRectEnabled = true;
-}
